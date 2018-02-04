469ec3d7375556ae9a0edb129fc63a256358ac16
QuqqU <rldnd913@naver.com>
2018-02-04 17:05:45 +0900

    make round corner

--- Omok/Menu/StartButton.swift
+++ Omok/Menu/StartButton.swift
@@ -16,19 +16,26 @@ class StartButton: UIButton {
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         
-        path.move(to: CGPoint(x: 0,
-                              y: LENGTH / 2))
-        path.addLine(to: CGPoint(x: LENGTH / 2,
-                                 y: 0))
-        path.addLine(to: CGPoint(x: LENGTH,
-                                 y: LENGTH / 2))
-        path.addLine(to: CGPoint(x: LENGTH / 2,
-                                 y: LENGTH))
-        path.addLine(to: CGPoint(x: 0,
-                                 y: LENGTH / 2))
-    
-        path.lineWidth = 3
-        UIColor.black.setStroke()
+        path.move(to: CGPoint(x: 5, y: LENGTH / 2 - 5))
+        
+        path.addLine(to: CGPoint(x: LENGTH / 2 - 5, y: 5))
+        path.addQuadCurve(to: CGPoint(x: LENGTH / 2 + 5, y: 5),
+                          controlPoint: CGPoint(x: LENGTH / 2, y: 0))
+        
+        path.addLine(to: CGPoint(x: LENGTH - 5, y: LENGTH / 2 - 5))
+        path.addQuadCurve(to: CGPoint(x: LENGTH - 5, y: LENGTH / 2 + 5),
+                          controlPoint: CGPoint(x: LENGTH, y: LENGTH / 2))
+        
+        path.addLine(to: CGPoint(x: LENGTH / 2 + 5, y: LENGTH - 5))
+        path.addQuadCurve(to: CGPoint(x: LENGTH / 2 - 5, y: LENGTH - 5),
+                          controlPoint: CGPoint(x: LENGTH / 2, y: LENGTH))
+
+        path.addLine(to: CGPoint(x: 5, y: LENGTH / 2 + 5))
+        path.addQuadCurve(to: CGPoint(x: 5, y: LENGTH / 2 - 5),
+                          controlPoint: CGPoint(x: 0, y: LENGTH / 2))
+        
+        path.lineWidth = 5
+        UIColor.white.setStroke()
         path.stroke()
     }
  
