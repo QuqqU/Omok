018b7103433bb3dcfc3a0a489d578ded82cb6aee
QuqqU <rldnd913@naver.com>
2018-02-04 15:07:54 +0900

    create Custom Button Swift File

---
+++ Omok/Menu/StartButton.swift
@@ -0,0 +1,21 @@
+//
+//  StartButton.swift
+//  Omok
+//
+//  Created by 정기웅 on 2018. 2. 4..
+//  Copyright © 2018년 정기웅. All rights reserved.
+//
+
+import UIKit
+
+class StartButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
